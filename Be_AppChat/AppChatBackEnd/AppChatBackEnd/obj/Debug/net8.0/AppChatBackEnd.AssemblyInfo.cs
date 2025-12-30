d633186099e7b87c6393c524f2197ea6a1d50884
SV-Thuc2003 <21130557@st.hcmuaf.edu.vn>
2025-12-30 13:44:35 +0700

    update code model

--- Be_AppChat/AppChatBackEnd/AppChatBackEnd/obj/Debug/net8.0/AppChatBackEnd.AssemblyInfo.cs
+++ Be_AppChat/AppChatBackEnd/AppChatBackEnd/obj/Debug/net8.0/AppChatBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppChatBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2005a97ebe6afc3cf76f2eb1dfc4b285a311442c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0925e0ab77f32e5c736a129d9312513a821f1353")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppChatBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppChatBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
