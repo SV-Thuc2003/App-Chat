8af0f68c09d627c8859a356578a480658d09a914
thinhmeo <21130546@st.hcmuaf.edu.vn>
2026-01-16 21:01:55 +0700

    Create_UserController
    Quản lý trạng thái người dùng
    BE

---
+++ Be_AppChat/AppChatBackEnd/AppChatBackEnd/Models/Entities/UserDetails.cs
@@ -0,0 +1,25 @@
+using AppChat.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppChatBackEnd.Models.Entities
+{
+    public class UserDetails
+    {
+        [Key]
+        public int UserDetailId { get; set; }
+        public DateTime? Dob { get; set; }
+        public int? Verified { get; set; }
+        public DateTime? OtpExpiryTime { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Otp { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Gender { get; set; }
+        public string? Status { get; set; } = "Active";
+        public int reportAmount { get; set; } = 0; 
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public Users User { get; set; }
+    }
+}
